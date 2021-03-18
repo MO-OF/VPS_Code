cba2f1e31f5e80f052fbb347f21eef45250c03ce
MO_OF <740816206@qq.com>
2021-03-18 21:28:26 +0800

    Update MO_server.sh

--- MO_server.sh
+++ MO_server.sh
@@ -1,1 +1,2 @@
-nohup python3 -u /root/GetBingWallpaper.py &> /root/BWall.out 2>&1 &
+nohup python3 -u /root/FileMonitor.py &> /root/FileMonitor.out 2>&1 &
+nohup python3 -u /root/FileShare.py &> /root/FileShare.out 2>&1 &
